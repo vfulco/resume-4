57f22abd5cc918f3e8f1ec9abc9bde2e5e0ec529
Phillip Ridlen <phillip@balcomagency.com>
2011-04-07 14:04:00 -0500

    refactor LatexEscape

--- helpers.rb
+++ helpers.rb
@@ -1,3 +1,22 @@
 def wget(url, file=nil)
   `wget --no-check-certificate #{"-O #{file}" if file } #{url}`
 end
+
+module LatexEscape
+  def latex_escape
+    return self.
+      to_s.
+      gsub(/&/, /\\&/.source).
+      gsub(/(LaTeX)/, '\\\\\1')
+  end
+  def le
+    self.latex_escape
+  end
+end
+
+class String
+  include LatexEscape
+end
+class Fixnum
+  include LatexEscape
+end
